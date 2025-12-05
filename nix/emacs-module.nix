8b22d3519ab53b1c325c8caa88c38639099d6725
JustaDebianDistro <thespacejamzdvd@gmail.com>
2025-12-05 03:08:18 -0500

    more with dev shells

--- nix/emacs-module.nix
+++ nix/emacs-module.nix
@@ -1,53 +1,84 @@
 { pkgs, zig }:
 let
-  emacsWithConfig = pkgs.emacsWithPackages {
-    config = ./emacs-config.el;
-    package = pkgs.emacs29;
-    alwaysEnsure = true;
-    extraEmacsPackages = epkgs: with epkgs;[
-      use-package
-      general
-
-      zig-mode
-      lsp-mode
-      lsp-ui
-      company
-      flycheck
-
-      projectile
-      magit
-      which-key
-
-      doom-themes
-      doom-modeline
-      all-the-icons
-      vertico
-      marginalia
-      orderless
-      consult
-
-      exec-path-from-shell
-      direnv
-
-    ];
-
-  };
+#  emacsWithConfig = pkgs.emacsWithPackages {
+#    config = ./emacs-config.el;
+#    package = pkgs.emacs29;
+#    alwaysEnsure = true;
+#    extraEmacsPackages = epkgs: with epkgs;[
+#      use-package
+#      general
+#
+#      zig-mode
+#      lsp-mode
+#      lsp-ui
+#      company
+#      flycheck
+#
+#      projectile
+#      magit
+#      which-key
+#
+#      doom-themes
+#      doom-modeline
+#      all-the-icons
+#      vertico
+#      marginalia
+#      orderless
+#      consult
+#
+#      exec-path-from-shell
+#      direnv
+#
+#    ];
+#
+#  };
   
-  emacsTools = with pkgs;[vterm emacs29Packages.pdf-tools emacs29Packages.org-roam];
+#  emacsTools = with pkgs;[vterm emacs29Packages.pdf-tools emacs29Packages.org-roam];
 
-  shellHook= ''
-    echo "  Emacs: ${emacsWithConfig.name}"
-    # Set up Emacs environment
-    export EMACSLOADPATH="${emacsWithConfig.deps}/share/emacs/site-lisp:$EMACSLOADPATH"
-    export PATH="${emacsWithConfig}/bin:$PATH"
-    
-    # For vterm in Emacs
-    export LIBRARY_PATH="${pkgs.vterm}/lib:$LIBRARY_PATH"
-    
-  '';
-in{
+#  shellHook= ''
+#    echo "  Emacs: ${emacsWithConfig.name}"
+#    # Set up Emacs environment
+#    export EMACSLOADPATH="${emacsWithConfig.deps}/share/emacs/site-lisp:$EMACSLOADPATH"
+#    export PATH="${emacsWithConfig}/bin:$PATH"
+#    
+#    # For vterm in Emacs
+#    export LIBRARY_PATH="${pkgs.vterm}/lib:$LIBRARY_PATH"
+#    
+#  '';
+#in{
   devShells.x86_64-linux = {
-    emacsconfig = pkgs.mkShell {packages = [emacsWithConfig] ++ emacsTools; inherit shellHook;};
+    #    emacsconfig = pkgs.mkShell {packages = [emacsWithConfig] ++ emacsTools; inherit shellHook;};
+    emacsconfig = pkgs.mkShell {
+      packages = [
+        # Simple emacsWithPackages - ALWAYS WORKS
+        (pkgs.emacsWithPackages (epkgs: with epkgs; [
+          use-package
+          general
+          zig-mode
+          lsp-mode
+          lsp-ui
+          company
+          flycheck
+          projectile
+          magit
+          which-key
+          doom-themes
+          doom-modeline
+          all-the-icons
+          vertico
+          marginalia
+          orderless
+          consult
+          exec-path-from-shell
+          direnv
+        ]))
+        pkgs.vterm
+        pkgs.emacs29Packages.pdf-tools
+        pkgs.emacs29Packages.org-roam
+      ];
+      shellHook = '' echo "emacs all packages loaded"'';
+
+    };
     emacslite = pkgs.mkShell {
       packages = [
         (pkgs.emacsWithPackages (epkgs: with epkgs; [
@@ -67,4 +98,4 @@ in{
 #
 #  };
 
-}
+#}
